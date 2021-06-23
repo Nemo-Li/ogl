bcff48ec0386d36f3cf4745428d59db6d293ce20
wangli <wangli@znds.com>
2021-06-23 09:49:38 +0800

    feature:test dds load 2

--- src/rendering/Texture.cpp
+++ src/rendering/Texture.cpp
@@ -8,10 +8,6 @@
 #include <iostream>
 #include <helpers/RootDir.h>
 
-#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
-#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
-#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
-
 Texture::Texture()
         : use_linear(true), to_id(0) {
 }
@@ -65,64 +61,83 @@ bool Texture::load(const std::string &file_name) {
 }
 
 void Texture::loadDds(const char *imagePath) {
-    unsigned char header[124];
+    // lay out variables to be used
+    unsigned char *header;
 
-    FILE *fp;
+    unsigned int width;
+    unsigned int height;
+    unsigned int mipMapCount;
 
-    /* try to open the file */
-    fp = fopen(imagePath, "rb");
-    if (fp == NULL) {
-        printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagePath);
-        getchar();
-        return;
-    }
-
-    /* verify the type of file */
-    char filecode[4];
-    fread(filecode, 1, 4, fp);
-    if (strncmp(filecode, "DDS ", 4) != 0) {
-        fclose(fp);
-        return;
-    }
-
-    /* get the surface desc */
-    fread(&header, 124, 1, fp);
-
-    unsigned int height = *(unsigned int *) &(header[8]);
-    unsigned int width = *(unsigned int *) &(header[12]);
-    unsigned int linearSize = *(unsigned int *) &(header[16]);
-    unsigned int mipMapCount = *(unsigned int *) &(header[24]);
-    unsigned int fourCC = *(unsigned int *) &(header[80]);
-
-    unsigned char *buffer;
-    unsigned int bufsize;
-    /* how big is it going to be including all mipmaps? */
-    bufsize = mipMapCount > 1 ? linearSize * 2 : linearSize;
-    buffer = (unsigned char *) malloc(bufsize * sizeof(unsigned char));
-    fread(buffer, 1, bufsize, fp);
-    /* close the file pointer */
-    fclose(fp);
-
-    unsigned int components = (fourCC == FOURCC_DXT1) ? 3 : 4;
+    unsigned int blockSize;
     unsigned int format;
-    switch (fourCC) {
-        case FOURCC_DXT1:
-            format = GL_VIEW_CLASS_S3TC_DXT1_RGBA;
-            break;
-        case FOURCC_DXT3:
-            format = GL_VIEW_CLASS_S3TC_DXT3_RGBA;
-            break;
-        case FOURCC_DXT5:
-            format = GL_VIEW_CLASS_S3TC_DXT5_RGBA;
-            break;
-        default:
-            free(buffer);
-            return;
-    }
 
+    unsigned int w;
+    unsigned int h;
+
+    unsigned char *buffer = 0;
+
+    // open the DDS file for binary reading and get file size
+    FILE *f;
+    if ((f = fopen(imagePath, "rb")) == 0)
+        return;
+    fseek(f, 0, SEEK_END);
+    long file_size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    // allocate new unsigned char space with 4 (file code) + 124 (header size) bytes
+    // read in 128 bytes from the file
+    header = static_cast<unsigned char *>(malloc(128));
+    fread(header, 1, 128, f);
+
+    // compare the `DDS ` signature
+    if (memcmp(header, "DDS ", 4) != 0)
+        return;
+
+    // extract height, width, and amount of mipmaps - yes it is stored height then width
+    height = (header[12]) | (header[13] << 8) | (header[14] << 16) | (header[15] << 24);
+    width = (header[16]) | (header[17] << 8) | (header[18] << 16) | (header[19] << 24);
+    mipMapCount = (header[28]) | (header[29] << 8) | (header[30] << 16) | (header[31] << 24);
+
+    // figure out what format to use for what fourCC file type it is
+    // block size is about physical chunk storage of compressed data in file (important)
+    if (header[84] == 'D') {
+        switch (header[87]) {
+            case '1': // DXT1
+                format = GL_VIEW_CLASS_S3TC_DXT1_RGBA;
+                blockSize = 8;
+                break;
+            case '3': // DXT3
+                format = GL_VIEW_CLASS_S3TC_DXT3_RGBA;
+                blockSize = 16;
+                break;
+            case '5': // DXT5
+                format = GL_VIEW_CLASS_S3TC_DXT5_RGBA;
+                blockSize = 16;
+                break;
+            case '0': // DX10
+                // unsupported, else will error
+                // as it adds sizeof(struct DDS_HEADER_DXT10) between pixels
+                // so, buffer = malloc((file_size - 128) - sizeof(struct DDS_HEADER_DXT10));
+            default:
+                return;
+        }
+    } else // BC4U/BC4S/ATI2/BC55/R8G8_B8G8/G8R8_G8B8/UYVY-packed/YUY2-packed unsupported
+        return;
+
+    // allocate new unsigned char space with file_size - (file_code + header_size) magnitude
+    // read rest of file
+    buffer = static_cast<unsigned char *>(malloc(file_size - 128));
+    if (buffer == 0)
+        return;
+    fread(buffer, 1, file_size, f);
+
+    // prepare new incomplete texture
     glGenTextures(1, &to_id);
+    if (to_id == 0)
+        return;
 
-    // "Bind" the newly created texture : all future texture functions will modify this texture
+    // bind the texture
+    // make it complete by specifying all needed parameters and ensuring all mipmaps are filled
     glBindTexture(GL_TEXTURE_2D, to_id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipMapCount - 1); // opengl likes array length of mipmaps
@@ -130,25 +145,34 @@ void Texture::loadDds(const char *imagePath) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // don't forget to enable mipmaping
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-    unsigned int blockSize = (format == GL_VIEW_CLASS_S3TC_DXT1_RGBA) ? 8 : 16;
+    // prepare some variables
     unsigned int offset = 0;
+    unsigned int size = 0;
+    w = width;
+    h = height;
 
-    /* load the mipmaps */
-    for (unsigned int level = 0; level < mipMapCount && (width || height); ++level) {
-        unsigned int size = ((width + 3) / 4) * ((height + 3) / 4) * blockSize;
-        glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height,
-                               0, size, buffer + offset);
-
+    // loop through sending block at a time with the magic formula
+    // upload to opengl properly, note the offset transverses the pointer
+    // assumes each mipmap is 1/2 the size of the previous mipmap
+    for (unsigned int i = 0; i < mipMapCount; i++) {
+        if (w == 0 || h == 0) { // discard any odd mipmaps 0x1 0x2 resolutions
+            mipMapCount--;
+            continue;
+        }
+        size = ((w + 3) / 4) * ((h + 3) / 4) * blockSize;
+        glCompressedTexImage2D(GL_TEXTURE_2D, i, format, w, h, 0, size, buffer + offset);
         offset += size;
-        width /= 2;
-        height /= 2;
-
-        // Deal with Non-Power-Of-Two textures. This code is not included in the webpage to reduce clutter.
-        if (width < 1) width = 1;
-        if (height < 1) height = 1;
+        w /= 2;
+        h /= 2;
     }
+    // discard any odd mipmaps, ensure a complete texture
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipMapCount - 1);
+    // unbind
+    glBindTexture(GL_TEXTURE_2D, 0);
 
+    // easy macro to get out quick and uniform (minus like 15 lines of bulk)
     free(buffer);
+    free(header);
+    fclose(f);
 }
