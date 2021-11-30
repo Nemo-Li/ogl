ff7b23e4058fcfff4bfae14296a1eaf7dbe086b2
wangli <wangli@znds.com>
2021-11-30 16:36:36 +0800

    feature:使用EBO，简单矩形

--- src/main.cpp
+++ src/main.cpp
@@ -40,9 +40,15 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "}";
 
 float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+};
+
+unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
 };
 
 int main() {
@@ -77,6 +83,7 @@ int main() {
 
     initShaderProgram();
     initVAO();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     // -----------
@@ -91,7 +98,8 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -131,15 +139,20 @@ void initShaderProgram() {
 
 void initVAO() {
     unsigned int VBO;
+    unsigned int EBO;
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    // 1. 绑定VAO
+    // 1. 绑定顶点数组对象
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    // 2. 把顶点数组复制到缓冲中供OpenGL使用
+    // 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 3. 设置顶点属性指针
+    // 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. 设定顶点属性指针
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
